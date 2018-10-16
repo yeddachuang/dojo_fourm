137c0bab8a6d02bb52a5acbfeb18cabbbdea554f
Yedda <YeddaChuang@gmail.com>
2018-10-17 03:04:07 +0800

    set route in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :posts do
+    get :feed
+  end
+  resource :users, only: [:show, :edit, :update]
+
+  namespace :admin do
+    resource :categories
+    resource :users, only: [:show, :update]
+    root "post#index"
+  end
+
+  root "posts#index"
 end
