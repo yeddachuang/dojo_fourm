0e0c399a6531539b384489de671dad04d8f68637
Yedda <YeddaChuang@gmail.com>
2018-10-17 02:04:55 +0800

    add attributes for user profile in user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_174711) do
+ActiveRecord::Schema.define(version: 2018_10_16_180205) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 2018_10_16_174711) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "intro"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
