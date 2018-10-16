1ce226393dccbb2b2b9d071dbfe2999a986f85e8
Yedda <YeddaChuang@gmail.com>
2018-10-17 01:20:28 +0800

    rails generate model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_171530) do
+ActiveRecord::Schema.define(version: 2018_10_16_171902) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
