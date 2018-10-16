4403bb3e913ce227417c095cef9e261e257d5889
Yedda <YeddaChuang@gmail.com>
2018-10-17 01:17:49 +0800

    rails genetate model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_170203) do
+ActiveRecord::Schema.define(version: 2018_10_16_171530) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
