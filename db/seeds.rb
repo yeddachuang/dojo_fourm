b58b83a5164396d48a71edaf374061d2804b3955
Yedda <YeddaChuang@gmail.com>
2018-10-18 02:14:17 +0800

    add seed category and default admin user in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category
+Category.destroy_all
+
+category_list = [
+  { name: "Cat" },
+  { name: "Dog" },
+  { name: "Hamster" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
+
+# Default admin
+User.create(
+  name: "Dojo",
+  email: "admin@example.com",
+  password: "12345678",
+  intro: "Dojo Fourm Admin",
+  role: "admin"
+)
+puts "Default admin created!"
